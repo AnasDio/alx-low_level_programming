4313eb1f6cb941ee7e2631920b9206119596463a
Anas <argusthedemon66@gmail.com>
2023-06-18 10:33:25 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - a program to output a text using printf
+ *
+ * Return:0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
