d2c3ebada171a44391a70122de0c3baa88a12002
Anas <argusthedemon66@gmail.com>
2023-06-20 14:20:32 -0700

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - mirror
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int a;
+for (a = 48; a <= 57; a++)
+{
+	putchar(a);
+}
+for (a = 97; a <= 102; a++)
+{
+	putchar(a);
+}
+putchar('\n');
+return (0);
+}
