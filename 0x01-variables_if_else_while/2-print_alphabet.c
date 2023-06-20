51979897984a4507f039bd02c0a6b4d64f1c99d4
Anas <argusthedemon66@gmail.com>
2023-06-20 06:13:51 -0700

    alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - alphabet
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+for (int a = 97; a <= 122; a++)
+{
+	printf("%c",
+	a);
+}
+printf("\n");
+return (0);
+}
