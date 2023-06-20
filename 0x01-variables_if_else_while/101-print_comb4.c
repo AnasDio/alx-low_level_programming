59033148ee128617825eeee1c4b0355ac6c4e1b1
Anas <argusthedemon66@gmail.com>
2023-06-20 16:36:43 -0700

    last

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - prints all combinations of three digits
+ * Return: ALways 0 (Success)
+ */
+int main(void)
+{
+int a, b, c;
+for (a = 48; a <= 56; a++)
+{
+	for (b = 49; b <= 57; b++)
+	{
+		for (c = 50; c <= 57; c++)
+		{
+			if (c > b && b > a)
+			{
+				putchar(a);
+				putchar(b);
+				putchar(c);
+				if (a != 55 || b != 56)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+}
+putchar('\n');
+return (0);
