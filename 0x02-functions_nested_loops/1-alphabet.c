c917adccf65ef3e87e0f6e10bfa371df6372c3db
Anas <argusthedemon66@gmail.com>
2023-06-21 09:06:37 -0700

    alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - a function to print alphabet
+ *
+ * Return:0 (Success)
+ */
+void print_alphabet(void)
+{
+int a;
+for (a = 97; a <= 122; a++)
+{
+	_putchar(a);
+}
+_putchar('\n');
+}
