88406fbaaa472dc508e386158f8498b2b660b000
Anas <argusthedemon66@gmail.com>
2023-06-21 09:25:37 -0700

    fiiiix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,9 +6,13 @@
  */
 void print_alphabet_x10(void)
 {
-int a;
+int a, b;
 for (a = 0; a < 10; a++)
 {
-	print_alphabet();
+	for (b = 97; b <= 122; b++)
+	{
+		_putchar(b);
+	}
+	_putchar('\n');
 }
 }
