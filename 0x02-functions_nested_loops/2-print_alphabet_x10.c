6f4341b47cdfd5ffdfd79ed6b02b38e8ea77422b
Anas <argusthedemon66@gmail.com>
2023-06-21 09:18:59 -0700

    alphabetX10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - just as the name says
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+int a;
+for (a = 0; a < 10; a++)
+{
+	print_alphabet();
+}
