9cd3b95fd089e24fd1eeefefea34f9a27db92cdf
Anas <argusthedemon66@gmail.com>
2023-06-21 15:56:11 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,6 +7,7 @@
  * Return: 1 if the char is upper case 0 if the char is lower
  */
 int _islower(int c);
+{
 if (c >= 97 && c <= 122)
 {
 	return (1);
@@ -14,4 +15,5 @@ if (c >= 97 && c <= 122)
 else
 {
 	return (0);
+}
 }
