6bd2e4a6578d4047e0d33918af50ef184c61ff0a
Anas <argusthedemon66@gmail.com>
2023-06-22 15:45:39 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an interger
+ *
+ * @int: the integer to be checked
+ *
+ * Return: 0 (Success)
+ */
+int _abs(int)
+{
+int a;
+if (a < 0)
+{
+	printf("%d\n",
+	a * -1);
+}
+else
+{
+	printf("%d\n",
+	a);
+}
+return (0);
+}
