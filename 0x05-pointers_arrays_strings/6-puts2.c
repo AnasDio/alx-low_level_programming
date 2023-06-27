64252c13a4460e8ffc1ad577c2fd72a8aeb15314
Anas <argusthedemon66@gmail.com>
2023-06-27 08:39:46 -0700

    print

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts2 - prints even strings
+ *
+ * @str: the string
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+int i = 0;
+while (*(str + i) != '\0')
+{
+	if (i % 2 == 0)
+	{
+		_putchar(*(str + i));
+	}
+i++
+}
+}
