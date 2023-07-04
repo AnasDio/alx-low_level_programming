ebd9b5bff38f4787bbc7c53a2f309d9ffe3d9ceb
Anas <argusthedemon66@gmail.com>
2023-07-04 15:14:38 -0700

    char

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strchr - function to search for a char
+ * @s: string
+ * @c: char
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+		else
+			return (0);
+}
