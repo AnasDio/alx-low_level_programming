ece5803b25bc80773305e70cd8c7f9ba6b9ab9b7
Anas <argusthedemon66@gmail.com>
2023-07-11 19:38:21 -0700

    freee

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_grid - free the memory
+ * @grid: 2d array
+ * @height: height
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	while (i < height)
+	{
+		free(*(grid + i));
+	}
+}
