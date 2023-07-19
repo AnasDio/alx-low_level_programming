7005fad9a383275e2acf792cbd94b39909645bfe
Anas <argusthedemon66@gmail.com>
2023-07-19 00:41:56 -0700

    dogs

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * init_dog - assign a var to struct dog
+ * @d: pointer of struct
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner name;
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	dog my_dog;
+}
