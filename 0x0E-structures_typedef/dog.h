4f6d28021e045f9c19e0675a2368590ada442988
Anas <argusthedemon66@gmail.com>
2023-07-18 23:55:59 -0700

    struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - give a dog info
+ * @name: the name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner
+ * Description: this struct gives information about dogs
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
